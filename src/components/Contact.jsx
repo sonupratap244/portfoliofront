b9a79260f290e4a7f18c4e707a8f15d7bf7497ae
Son Pratap <117804069+sonupratap244@users.noreply.github.com>
2025-05-29 10:33:39 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,7 +18,7 @@ function Contact() {
       message: data.message,
     };
     try {
-      await axios.post("http://localhost:8000/api/create", userInfo);
+      await axios.post("https://portfoliobackend-w4rb.onrender.com", userInfo);
       toast.success("Your message has been sent");
     } catch (error) {
       toast.error("Something went wrong");
