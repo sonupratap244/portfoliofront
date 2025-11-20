5d8ff4ad8b17c19a16ddb8bd6b3cbda80da83339
Son Pratap <117804069+sonupratap244@users.noreply.github.com>
2025-11-20 12:10:49 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Change if deployed
+  baseURL: "https://portfolioback-manjeet.onrender.com/api", // Change if deployed
 });
 
 API.interceptors.request.use((req) => {
